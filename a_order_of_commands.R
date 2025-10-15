a3f2b3ad41475163953ff6812d6ce2dcf0252407
syutkachenko <syutkachenko@gmail.com>
2025-10-15 15:59:49 +0100

    Homework3

--- a_order_of_commands.R
+++ a_order_of_commands.R
@@ -3,7 +3,7 @@ rm(list = ls())
 ## 1 ----
 aa <- 1
 bb <- 2
-cc <- aa + 66
+cc <- aa + bb
 
 stopifnot(cc == 3)
 
@@ -12,6 +12,7 @@ print("Steps 1/5 passed")
 ## 2 ----
 
 bb <- 11
+aa <- 11
 cc <- aa
 
 stopifnot(bb == cc)
@@ -27,7 +28,7 @@ print("Steps 3/5 passed")
 
 ## 4 ----
 
-aa <- aa - 1
+aa <- aa - 9
 
 stopifnot(aa == 2)
 
@@ -35,7 +36,7 @@ print("Steps 4/5 passed")
 
 ## 5 ----
 
-cc <- aa + bb
+cc <- aa
 
 stopifnot(cc == 2)
 
