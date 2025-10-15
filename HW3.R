c7df430cb39f0007e0bd49111659fd6707ae1641
Tana-Gab <tani.gabor@gmail.com>
2025-10-15 15:23:43 +0100

    Adjustments to the t_gaborova branch

---
+++ HW3.R
@@ -0,0 +1,42 @@
+rm(list = ls())
+
+## 1 ----
+aa <- 1
+bb <- 2
+cc <- aa + bb
+
+stopifnot(cc == 3)
+
+print("Steps 1/5 passed")
+
+## 2 ----
+
+bb <- 1
+cc <- 1
+
+stopifnot(bb == cc)
+
+print("Steps 2/5 passed")
+
+## 3 ----
+
+stopifnot(cc == aa)
+
+print("Steps 3/5 passed")
+
+
+## 4 ----
+
+aa <- aa + 1
+
+stopifnot(aa == 2)
+
+print("Steps 4/5 passed")
+
+## 5 ----
+
+cc <- aa + bb - 1
+
+stopifnot(cc == 2)
+
+print("Step 5/5 passed")
