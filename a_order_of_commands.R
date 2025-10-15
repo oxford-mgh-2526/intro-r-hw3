0815086e5b2403eaddb99a4160ae577e83a1aacc
josephkalibbala <joseph.kalibbala@gtc.ox.ac.uk>
2025-10-15 17:23:48 +0100

    Commit 3 HW3

--- a_order_of_commands.R
+++ a_order_of_commands.R
@@ -1,8 +1,8 @@
 rm(list = ls())
 
 ## 1 ----
-aa <- 3
-bb <- 0
+aa <- 1
+bb <- 2
 cc <- aa + bb
 
 stopifnot(cc == 3)
@@ -11,7 +11,7 @@ print("Steps 1/5 passed")
 
 ## 2 ----
 
-bb <- 11
+bb <- 11 - 10
 cc <- bb
 
 stopifnot(bb == cc)
@@ -27,7 +27,7 @@ print("Steps 3/5 passed")
 
 ## 4 ----
 
-aa <- aa - 1
+aa <- aa + 1
 
 stopifnot(aa == 2)
 
@@ -35,7 +35,7 @@ print("Steps 4/5 passed")
 
 ## 5 ----
 
-cc <- aa + bb
+cc <- aa + bb -1
 
 stopifnot(cc == 2)
 
